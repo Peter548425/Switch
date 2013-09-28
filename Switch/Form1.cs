2bf7497e8565d0e6fd5012459ebcf2845860e564
Peter548425 <peto.protus@gmail.com>
2013-09-28 18:21:05 +0200

    28.9.2013

---
+++ Switch/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SharpPcap;
+using SharpPcap.LibPcap;
+using System.IO;
+
+namespace Switch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Device list
+            CaptureDeviceList devices = CaptureDeviceList.Instance;
+
+            if (devices.Count < 1)
+            {
+                richTextBox1.AppendText("Žiadne zariadenia!");
+            }
+                
+            foreach (ICaptureDevice dev in devices)
+            {
+                richTextBox1.AppendText(dev.Description.ToString() + "\n");
+                
+            }
+
+        }
+       
+    }
+}
